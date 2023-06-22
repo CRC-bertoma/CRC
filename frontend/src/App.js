ea708fa25ced84a35b856f2d2f6e38abc66905dd
Marco Bertolino <marcobertolino.developer@gmail.com>
2023-06-22 15:07:04 +0200

    portfolio fixies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
-import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import TopButton from "./components/topButton/TopButton";
